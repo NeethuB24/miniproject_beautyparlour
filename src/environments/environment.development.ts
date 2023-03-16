43e49d3537ae8b949865fe70398cf7a553873579
Neethu <neethuskb2000gmail.com>
2023-03-16 15:10:31 +0530

    compl

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,10 +1,10 @@
 export const environment = {
   firebase: {
     projectId: 'mini-beautyparlour',
-    appId: '1:85257830718:web:2c8cd77eb68bc71992ac6f',
+    appId: '1:85257830718:web:3b8b9c58386cfa4e92ac6f',
     storageBucket: 'mini-beautyparlour.appspot.com',
     apiKey: 'AIzaSyDYC7YteeVhuqIhNNuyMs1-Xup5iF6qMbo',
     authDomain: 'mini-beautyparlour.firebaseapp.com',
     messagingSenderId: '85257830718',
-    measurementId: 'G-TDKRF0XWXH',
+    measurementId: 'G-4D21T5RDPK',
   },};
